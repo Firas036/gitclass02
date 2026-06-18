b9786afab82690b8b17bfba4af97d25f42e0ce63
Firas036 <mardinifiras@gmail.com>
2026-06-18 13:31:29 +0200

    first commit

---
+++ src/test/java/test1.java
@@ -0,0 +1,10 @@
+public class test1 {
+    static void main() {
+
+
+        System.out.println("hello world");
+        System.out.println("this is the project version 1");
+
+    }
+}
+//done with version 1
